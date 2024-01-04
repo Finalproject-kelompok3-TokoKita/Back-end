07b5d9492f227f4bac4c84523da6685caa6872ab
Muhammad Ariansyah <115616443+AriiAnn@users.noreply.github.com>
2024-01-04 22:24:07 +0700

    up

--- controller/UserController.js
+++ controller/UserController.js
@@ -15,10 +15,7 @@ export const getUsers = async (req, res) => {
 
 export const Register = async (req, res) => {
   const { name, email, password, confPassword } = req.body;
-  if (password !== confPassword)
-    return res
-      .status(400)
-      .json({ msg: "Password dan Confirm Password Tidak Cocok" });
+  if (password !== confPassword) return res.status(400).json({ msg: "Password dan Confirm Password Tidak Cocok" });
   const salt = await bcrypt.genSalt();
   const hashPassword = await bcrypt.hash(password, salt);
   try {
@@ -45,20 +42,12 @@ export const Login = async (req, res) => {
     const userId = user[0].id;
     const name = user[0].name;
     const email = user[0].email;
-    const accessToken = jwt.sign(
-      { userId, name, email },
-      process.env.ACCESS_TOKEN_SECRET,
-      {
-        expiresIn: "30s",
-      }
-    );
-    const refreshToken = jwt.sign(
-      { userId, name, email },
-      process.env.REFRESH_TOKEN_SECRET,
-      {
-        expiresIn: "1d",
-      }
-    );
+    const accessToken = jwt.sign({ userId, name, email }, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "30s",
+    });
+    const refreshToken = jwt.sign({ userId, name, email }, process.env.REFRESH_TOKEN_SECRET, {
+      expiresIn: "1d",
+    });
     await Users.update(
       { refresh_token: refreshToken },
       {
@@ -77,21 +66,24 @@ export const Login = async (req, res) => {
   }
 };
 
-export const Logout = async(req, res) => {
+export const Logout = async (req, res) => {
   const refreshToken = req.cookies.refreshToken;
-  if(!refreshToken) return res.sendStatus(204);
+  if (!refreshToken) return res.sendStatus(204);
   const user = await Users.findAll({
     where: {
       refresh_token: refreshToken,
     },
   });
   if (!user[0]) return res.sendStatus(204);
-  const userId = user[0].id
-  await Users.update({refresh_token: null}, {
-    where:{
-      id: userId
+  const userId = user[0].id;
+  await Users.update(
+    { refresh_token: null },
+    {
+      where: {
+        id: userId,
+      },
     }
-  })
-  res.clearCookie('refreshToken')
-  return res.sendStatus(200)
+  );
+  res.clearCookie("refreshToken");
+  return res.sendStatus(200);
 };
