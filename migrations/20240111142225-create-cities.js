a987185ed58fc356bbde1ce06ecb5e316a6322bf
Muhammad Ariansyah <115616443+AriiAnn@users.noreply.github.com>
2024-01-11 22:23:45 +0700

    up

---
+++ migrations/20240111142225-create-cities.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('cities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      provinceId: {
+        type: Sequelize.INTEGER,
+        references: {
+          key: 'id',
+          model: 'provinces',
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('cities');
+  }
+};
