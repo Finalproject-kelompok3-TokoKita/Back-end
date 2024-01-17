73e4dfe5e1c72d7c982d91b016bd7de069219d29
rzkytmgr <rzkytmgr@gmail.com>
2024-01-17 10:05:48 +0700

    refresh token for authentication

--- routes/cities.routes.js
+++ routes/cities.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
+const authMiddleware = require('../middlewares/auth.middleware')
 const router = Router();
 
 const { CitiesController } = require('../controllers');
 
-router.get('/cities', CitiesController.getAll);
+router.get('/cities', authMiddleware, CitiesController.getAll);
 router.get('/cities/:id(\\d+)', CitiesController.getOne);
 router.post('/cities', CitiesController.createOne);
 router.put('/cities/:id(\\d+)', CitiesController.updateOne);
