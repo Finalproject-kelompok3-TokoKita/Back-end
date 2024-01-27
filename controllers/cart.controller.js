4657f81df57237abcc04657bccf81789497bea30
Arista Abdurrahman <aristaabdurrahman@gmail.com>
2024-01-27 23:30:19 +0700

    up

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,4 +1,4 @@
-const { cart, products } = require("../models");
+const { cart, products, } = require("../models");
 const { BadRequestError, DataNotFoundError } = require("../utils/errors");
 
 const getAll = async (req, res, next) => {
@@ -19,11 +19,47 @@ const getAll = async (req, res, next) => {
   }
 };
 
+// const createOne = async (req, res, next) => {
+//   try {
+//     const { productId } = req.body;
+//     const quantity = 1;
+
+//     const Cart = await cart.findOne({
+//       where: {
+//         productId: productId,
+//         userId: req.user.id,
+//       },
+//     });
+
+//     if (Cart) {
+//       throw new BadRequestError("Tidak boleh menambahkan product yang sudah di keranjang");
+//     }
+
+//     const cartCreated = await cart.create({
+//       userId: req.user.id,
+//       productId,
+//       quantity,
+//     });
+
+//     const Product = await products.findOne({
+//       where: {
+//         id: cartCreated.id,
+//       },
+//     });
+
+//     return res.status(201).json({
+//       message: "Created",
+//       data: Product,
+//     });
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
 const createOne = async (req, res, next) => {
   try {
     const { productId } = req.body;
     const quantity = 1;
-
     const Cart = await cart.findOne({
       where: {
         productId: productId,
@@ -34,7 +70,6 @@ const createOne = async (req, res, next) => {
     if (Cart) {
       throw new BadRequestError("Tidak boleh menambahkan product yang sudah di keranjang");
     }
-
     const cartCreated = await cart.create({
       userId: req.user.id,
       productId,
@@ -55,6 +90,7 @@ const createOne = async (req, res, next) => {
     next(err);
   }
 };
+
 const updateOne = async (req, res, next) => {
   try {
     const { productId, quantity } = req.body;
