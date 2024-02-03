6f2b4116c54d188b696b8bcb8554e3a46df75b5f
Arista Abdurrahman <aristaabdurrahman@gmail.com>
2024-02-03 14:28:08 +0700

    update seeder

---
+++ seeders/20240203063908-products.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('products', [
+      {
+        name: "Nasi Goreng",
+        description: "Nasi Goreng Spesial",
+        price: "15000",
+        quantity: "30",
+        storeId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
